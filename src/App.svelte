c2fb6a3641e9bd8d49b4d83547f53009434ed025
rightstartwp <moshe@rightstartwp.com>
2019-11-12 05:15:45 -0500

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  // Imports
+  import Tailwindcss from "./Tailwindcss.svelte";
+
+  // Exports
+  export let name;
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<Tailwindcss />
+<h1>Hello {name}!</h1>
